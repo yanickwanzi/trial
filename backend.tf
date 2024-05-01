61c19d15c5f42dafaaeaabb9e06e8f64c5bddd45
Ndiforamang Fusi <fusisoft@gmail.com>
2024-05-01 10:42:57 -0500

    added remote state file bucket

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "fusisoft-terraform-backend"
     key    = "infra_resources/terraform.tfstate"
-    region = "us-west-3"
+    region = "us-west-2"
     dynamodb_table = "s3-state-lock"
   }
 }
