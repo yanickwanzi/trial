292fa3c92511c4d4f2f28989ba0d5805acab9d97
Ndiforamang Fusi <fusisoft@gmail.com>
2024-05-10 23:14:29 -0500

    added eks module

--- providers.tf
+++ providers.tf
@@ -2,13 +2,12 @@ required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.7.0"
-    }
-}
-   
+    } 
 provider "aws" {
   profile = var.profile
   region  = var.main-region
   alias   = "shi-awslab"
+}
 }
 
 provider "kubernetes" {
