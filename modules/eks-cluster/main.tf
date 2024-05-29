6f7df2c57b5db12c2a33f8256a89c118e2c13d6c
Ndiforamang Fusi <fusisoft@gmail.com>
2024-05-29 06:57:21 -0500

    Added 1 worker node

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -51,7 +51,7 @@ module "eks" {
     node-group-02 = {
       min_size     = 1
       max_size     = 10
-      desired_size = 2
+      desired_size = 3
 
       instance_types = ["t3.large"]
       capacity_type  = "SPOT"
